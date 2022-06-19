81eb243fe356df6f938cf20a3b79d69df44ace3a
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-06-19 23:10:45 +0530

    Appdrive

--- bot/__init__.py
+++ bot/__init__.py
@@ -353,6 +353,16 @@ try:
         raise KeyError
 except:
     CRYPT = None
+   
+try:
+    APPDRIVE_EMAIL = getConfig('APPDRIVE_EMAIL')
+    APPDRIVE_PASS = getConfig('APPDRIVE_PASS')
+    if len(APPDRIVE_EMAIL) == 0 or len(APPDRIVE_PASS) == 0:
+        raise KeyError
+except KeyError:
+    APPDRIVE_EMAIL = None
+    APPDRIVE_PASS = None
+    
 try:
     HEROKU_APP_NAME = getConfig('HEROKU_APP_NAME')
     if len(HEROKU_APP_NAME) == 0:
