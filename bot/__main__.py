90cb5b7b3d7242831509063b946d5fa836664117
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-06-19 22:44:26 +0530

    Heroku

--- bot/__main__.py
+++ bot/__main__.py
@@ -22,7 +22,7 @@ from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editM
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss, usage
 
 now = datetime.now(pytz.timezone(f'{TIMEZONE}'))
 IMAGE_X = f"{IMAGE_URL}"
