d38c76adc8f7a843cf79d4663e3248640c5beda8
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-06-20 00:34:13 +0530

    g

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -106,10 +106,7 @@ class TgUploader:
                                                          supports_streaming=True,
                                                          disable_notification=True,
                                                          progress=self.__upload_progress)
-                    try:
-                        bot.send_video(self.__listener.message.from_user.id, video=self.__sent_msg.video.file_id, caption=cap_mono)
-                    except Exception as err:
-                        LOGGER.error(f"Failed to log to channel:\n{err}")
+                    
                 elif file_.upper().endswith(AUDIO_SUFFIXES):
                     duration , artist, title = get_media_info(up_path)
                     self.__sent_msg = self.__sent_msg.reply_audio(audio=up_path,
