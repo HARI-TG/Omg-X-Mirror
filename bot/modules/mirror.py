766a7d8054186128461fd125f362762d8c6c33dd
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-06-20 14:52:24 +0530

    Fixed

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -232,11 +232,11 @@ class MirrorListener:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
                 msg += f'\n\n𝗥𝗲𝗾𝘂𝗲𝘀𝘁𝗲𝗱 𝗕𝗬: {uname}'
-                msg += f"𝗧𝗶𝗺𝗲 𝗘𝗹𝗮𝗽𝘀𝗲𝗱: <code>{get_readable_time(time() - self.message.date.timestamp())}</code>\n\n"
+                msgt = f"\n\n𝗧𝗶𝗺𝗲 𝗘𝗹𝗮𝗽𝘀𝗲𝗱: <code>{get_readable_time(time() - self.message.date.timestamp())}</code>"
                 msg_g = f"\n\n - 𝗗𝗼𝗻'𝘁 𝗦𝗵𝗮𝗿𝗲 𝘁𝗵𝗲 𝗜𝗻𝗱𝗲𝘅 𝗟𝗶𝗻𝗸"
                 fwdpm = f"\n\n𝙄'𝙫𝙚 𝙎𝙚𝙣𝙙 𝙩𝙝𝙚 𝙇𝙞𝙣𝙠𝙨 𝙏𝙤 𝙔𝙤𝙪𝙧 𝙋𝙈 & 𝙇𝙤𝙜 𝘾𝙝𝙖𝙣𝙣𝙚𝙡"
             sendLog(msg + msg_g, self.bot, self.message, InlineKeyboardMarkup(buttons.build_menu(2)))
-            auto = sendMessage(msg + fwdpm, self.bot, self.message)
+            auto = sendMessage(msg + msgt + fwdpm, self.bot, self.message)
             Thread(target=auto_delete, args=(self.bot, self.message, auto)).start()
             sendPrivate(msg + msg_g, self.bot, self.message, InlineKeyboardMarkup(buttons.build_menu(2)))
             if self.isQbit and QB_SEED and not self.extract:
