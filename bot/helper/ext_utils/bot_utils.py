53609aa5f364491808a2e087395de8871994759f
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-06-20 13:47:06 +0530

    fixed

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen
 from telegram import InlineKeyboardMarkup
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR, LOGGER, status_reply_dict, status_reply_dict_lock, dispatcher, bot, 
+from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR, LOGGER, status_reply_dict, status_reply_dict_lock, dispatcher, bot 
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
