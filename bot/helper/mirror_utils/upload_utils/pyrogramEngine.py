2fe7f8f2a6584c250572fb4bdc4d464cb03b1b86
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-07-14 10:49:42 +0530

    try

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -66,7 +66,9 @@ class TgUploader:
         if file_.startswith('www'):  
             file_ = ' '.join(file_.split()[1:])
             file_ = '@MSPmoviesOffl ' + file_.strip('-')
-            file_ = f"{file_}"
+            new_path = ospath.join(dirpath, file_)
+            osrename(up_path, new_path)
+            up_path = new_path
         else:
             cap_mono = f"<code>{file_}</code>"
             pm_cap = f"<b>{file_}</b>"
