27e68f12fcc460b43fdffd0f999d5866d923320e
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-07-14 22:48:41 +0530

    Later

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -193,9 +193,6 @@ class MirrorListener:
     def onUploadComplete(self, link: str, size, files, folders, typ, name: str):
         if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
             DbManger().rm_complete_task(self.message.link)
-        if name.startswith('www'):  
-            name = ' '.join(name.split()[1:])
-            name = '@MSPmoviesOffl' + name.strip('-')
         msg = f"𝗡𝗮𝗺𝗲: <code>{escape(name)}</code>\n\n𝗦𝗶𝘇𝗲: {size}"
         if self.isLeech:
             msg += f'\n𝗧𝗼𝘁𝗮𝗹 𝗙𝗶𝗹𝗲𝘀: {folders}'
